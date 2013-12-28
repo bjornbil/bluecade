77628b0c4eb9b6ad84f5d7afce1b2a25afdd016b
Armarr <deprins.r@gmail.com>
2013-12-28 12:56:53 +0100

    Added DrawingPanel

---
+++ src/khl/mobile/bluecade/ui/games/pictionary/DrawingPanel.java
@@ -0,0 +1,131 @@
+package khl.mobile.bluecade.ui.games.pictionary;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class DrawingPanel extends View implements MotionEventListener{
+	
+	private Canvas drawCanvas;
+	private	Bitmap canvasBitmap;
+	private	Path drawPath = new Path();
+	private Paint drawPaint = new Paint();
+	
+	//Default behavior is Local, acts as expected, not used for multiplayer games
+	//Sender behavior also sends out events to its listeners, don't forget to register
+	// the multiplayer handler as a listener
+	//Reciever behavior ignores local touches, don't forget to register this
+	// DrawingPanel as a listener of the multiplayer handler
+	public static final int BEHAVIOR_LOCAL = 0;
+	public static final int BEHAVIOR_SENDER = 1;
+	public static final int BEHAVIOR_RECIEVER = 2;
+	
+	private int behavior = BEHAVIOR_LOCAL;
+	private List<MotionEventListener> listeners = new ArrayList<MotionEventListener>();
+	
+	//Because the drawing panel might be a different size on the screen of your opponent
+	// we have to scale the coordinates of incoming events.
+	// Only used with RECIEVER behavior
+	private double hScale = 1;
+	private double vScale = 1;
+	
+	public DrawingPanel(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		setColor(Color.BLACK);
+		drawPaint.setAntiAlias(true);
+		drawPaint.setStyle(Paint.Style.STROKE);
+		drawPaint.setStrokeJoin(Paint.Join.ROUND);
+		drawPaint.setStrokeCap(Paint.Cap.ROUND);
+	}
+	
+	public DrawingPanel(Context context, AttributeSet attrs, int behavior) {
+		this(context, attrs);
+		setBehavior(behavior);
+	}
+	
+	public void setBehavior(int behavior){
+		this.behavior = behavior;
+	}
+
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+		clear();
+		drawCanvas = new Canvas(canvasBitmap);
+		super.onSizeChanged(w, h, oldw, oldh);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		canvas.drawBitmap(canvasBitmap, 0, 0, null);
+		canvas.drawPath(drawPath, drawPaint);
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent e) {
+		if(this.behavior == this.BEHAVIOR_RECIEVER) return false;
+		boolean changed = (e.getAction() == MotionEvent.ACTION_DOWN ||
+				e.getAction() == MotionEvent.ACTION_MOVE ||
+				e.getAction() == MotionEvent.ACTION_UP);
+		if(changed){
+			if(this.behavior == this.BEHAVIOR_SENDER){
+				for(MotionEventListener l : this.listeners){
+					l.update(e);
+				}
+			}
+			this.update(e);
+		}
+		
+		return changed;
+	}
+	
+	public void setColor(int color){
+		drawPaint.setColor(color);
+		drawPaint.setStrokeWidth(20);
+	}
+	
+	public void eraseTool(){
+		drawPaint.setColor(Color.WHITE);
+		drawPaint.setStrokeWidth(40);
+	}
+	
+	public void clear(){
+		canvasBitmap.eraseColor(Color.WHITE);
+		invalidate();
+	}
+	
+	public void addMotionEventListener(MotionEventListener l){
+		this.listeners.add(l);
+	}	
+	
+	public void removeMotionEventListener(MotionEventListener l){
+		this.listeners.remove(l);
+	}
+
+	@Override
+	public void update(MotionEvent e) {
+		switch(e.getAction()){
+		case MotionEvent.ACTION_DOWN:
+			drawPath.moveTo(e.getX(), e.getY());
+			break;
+		case MotionEvent.ACTION_MOVE:
+			drawPath.lineTo(e.getX(), e.getY());
+			break;
+		case MotionEvent.ACTION_UP:
+			drawCanvas.drawPath(drawPath, drawPaint);
+			drawPath.reset();
+			break;
+		}
+		invalidate();
+	}
+}
---
+++ src/khl/mobile/bluecade/ui/games/pictionary/MotionEventListener.java
@@ -0,0 +1,9 @@
+package khl.mobile.bluecade.ui.games.pictionary;
+
+import android.view.MotionEvent;
+
+public interface MotionEventListener {
+
+	public void update(MotionEvent e);
+	
+}
