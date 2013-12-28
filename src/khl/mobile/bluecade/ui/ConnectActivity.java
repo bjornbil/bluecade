c889c37f09cb2db8474c15f47745928873080681
bjornbil <bjorn.billen@student.khleuven.be>
2013-12-28 12:30:29 +0100

    added paired device list to connectactivity

--- src/khl/mobile/bluecade/ui/ConnectActivity.java
+++ src/khl/mobile/bluecade/ui/ConnectActivity.java
@@ -39,10 +39,10 @@ public class ConnectActivity extends Activity {
 	public static String EXTRA_DEVICE_ADDRESS = "device_address";
 	private BluetoothAdapter mBtAdapter;
 	private BluetoothSocket socket;
-	private ArrayAdapter<String> devicesArrayAdapter;
+	private ArrayAdapter<String> devicesArrayAdapter, pairedDevicesArrayAdapter;
 	private Button scanButton, discoverButton;
 	private TextView title;
-	private ListView devices;
+	private ListView paireddevices,devices;
 	private int gameid;
 	private ArrayList<BluetoothDevice> availabledevices = new ArrayList<BluetoothDevice>();
 
@@ -71,20 +71,39 @@ public class ConnectActivity extends Activity {
 			}
 		});
 
-		devicesArrayAdapter = new ArrayAdapter<String>(this,
-				R.layout.device_name);
+		devicesArrayAdapter = new ArrayAdapter<String>(this,R.layout.device_name);
+		pairedDevicesArrayAdapter = new ArrayAdapter<String>(this,R.layout.device_name);
 
 		devices = (ListView) findViewById(R.id.new_devices);
 		devices.setAdapter(devicesArrayAdapter);
 		devices.setOnItemClickListener(mDeviceClickListener);
-
+		
+		paireddevices = (ListView) findViewById(R.id.paired_devices);
+		paireddevices.setAdapter(pairedDevicesArrayAdapter);
+		paireddevices.setOnItemClickListener(mDeviceClickListener);
+		
 		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
 		this.registerReceiver(mReceiver, filter);
 
 		filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
 		this.registerReceiver(mReceiver, filter);
-
 		mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+		 // Get a set of currently paired devices
+        Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
+        System.out.println(pairedDevices.size());
+
+        // If there are paired devices, add each one to the ArrayAdapter
+        if (pairedDevices.size() > 0) {
+            findViewById(R.id.title_paired_devices).setVisibility(View.VISIBLE);
+            for (BluetoothDevice device : pairedDevices) {
+                pairedDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        } else {
+            String noDevices = getResources().getText(R.string.none_paired).toString();
+            pairedDevicesArrayAdapter.add(noDevices);
+        }
+
+		
 	}
 
 	@Override
@@ -114,9 +133,9 @@ public class ConnectActivity extends Activity {
 		public void onItemClick(AdapterView<?> av, View v, int index, long arg3) {
 			mBtAdapter.cancelDiscovery();
 
-			//String info = ((TextView) v).getText().toString();
-			//String address = info.substring(info.length() - 17);
-			//final String name = info.substring(0, info.length() - 18);
+			// String info = ((TextView) v).getText().toString();
+			// String address = info.substring(info.length() - 17);
+			// final String name = info.substring(0, info.length() - 18);
 
 			AsyncTask<Integer, Void, Void> connectTask = new AsyncTask<Integer, Void, Void>() {
 				@Override
@@ -141,6 +160,7 @@ public class ConnectActivity extends Activity {
 				}
 			};
 			title.setText("Connecting...");
+			findViewById(R.id.title_new_devices).setVisibility(View.VISIBLE);
 			connectTask.execute(index);
 		}
 	};
@@ -153,12 +173,9 @@ public class ConnectActivity extends Activity {
 			if (BluetoothDevice.ACTION_FOUND.equals(action)) {
 
 				title.setText("Available devices");
-				findViewById(R.id.title_new_devices)
-						.setVisibility(View.VISIBLE);
-				BluetoothDevice device = intent
-						.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-				if (device.getBondState() != BluetoothDevice.BOND_BONDED
-						&& !availabledevices.contains(device)) {
+				findViewById(R.id.title_new_devices).setVisibility(View.VISIBLE);
+				BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+				if (device.getBondState() != BluetoothDevice.BOND_BONDED && !availabledevices.contains(device)) {
 					availabledevices.add(device);
 					devicesArrayAdapter.add(device.getName() + "\n"
 							+ device.getAddress());
